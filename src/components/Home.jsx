ad1cd08d1d9960710ed4e6f873b0204b2af27f2d
dustin1818 <dustinamoda18@gmail.com>
2023-07-03 22:36:20 +0800

    add showing movies

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [showingMovies, setShowingMovies] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTIyZGQ1NjIwN2QzZmU5ODMyNTI1NDEwZWQ3NDZmMiIsInN1YiI6IjYxMDkxMWExMmY4ZDA5MDA0OGU5ZWQ5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EL9RLiEv0kAhIhDOY0UnQkka_X4fTF5Lqa10DPFJBNg",
+      },
+    };
+
+    let API_URL =
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+
+    const fetchMovies = async () => {
+      const response = await fetch(API_URL, options);
+      if (!response.ok) throw new Error("Network response was not ok");
+      const data = await response.json();
+      setShowingMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
+
+  console.log(showingMovies);
+
   return (
     <div className="max-w-[1640px] mx-auto p-4">
       <div className="max-h-[500px] relative">
@@ -13,15 +39,37 @@ const Home = () => {
           </h1>
         </div>
         <img
-          className="w-full max-h-[500px] object-cover"
+          className="w-full max-h-[300px] object-cover"
           src="https://images.pexels.com/photos/8263349/pexels-photo-8263349.jpeg"
           alt=""
         />
       </div>
       <div className="mt-10">
-        <h1 className="text-3xl ">
-          <span className="font-bold">Trending </span> Movies
+        <h1 className="text-2xl sm:text-3xl mb-5">
+          <span className="font-bold">Showing </span> Movies
         </h1>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
+          {showingMovies.map((movies, index) => (
+            <div
+              className="flex flex-col gap-3 bg-white shadow-md rounded"
+              key={index}
+            >
+              <div className="w-100 rounded">
+                <img
+                  className="rounded-t object-cover"
+                  src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
+                  alt={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
+                />
+              </div>
+
+              <div className="px-4 py-2">
+                <h1 className="font-bold mb-3">{movies.original_title}</h1>
+                <p>{movies.overview}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
